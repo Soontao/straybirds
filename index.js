669dc05b137ea609c7e061642096f7a4c362969b
Theo Sun <theo.sun@sap.com>
2022-03-09 12:11:18 +0800

    chore: update

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 #!/usr/bin/env node
 
-var chapters = require('./stray.json');
-var process = require('process');
-var length = Object.keys(chapters).length;
+require("colors")
+const axios = require("axios").default
+const process = require('process');
+const os = require("os")
+const path = require("path")
+const fs = require("fs")
 
-var random = function (max) {
-  return Math.ceil(Math.random() * max);
+const CONTENT_URI = "https://cdn.jsdelivr.net/gh/Soontao/straybirds/stray.json"
+
+const LOCAL_PATH = path.join(os.tmpdir(), "stray_birds_tmp.json");
+
+/**
+ * get chapters from local or remote
+ * 
+ * @returns 
+ */
+async function getChapters() {
+
+  if (!fs.existsSync(LOCAL_PATH)) {
+    const { data } = await axios.get(CONTENT_URI)
+    fs.writeFileSync(LOCAL_PATH, JSON.stringify(data), { encoding: "utf-8" })
+  }
+  return JSON.parse(fs.readFileSync(LOCAL_PATH, { encoding: "utf-8" }))
 }
 
-var random_chapter = function (init) {
-  var thisIdx = init <= length ? init : random(length);
-  var thisChapter = chapters[thisIdx];
+/**
+ * ramdom value
+ * 
+ * @param {number} max max value
+ * @returns 
+ */
+const random = function (max) {
+  return Math.floor(Math.random() * max);
+}
+
+/**
+ * export a random chapter
+ * 
+ * @returns 
+ */
+const random_chapter = function (chapters, init) {
+  const length = Object.keys(chapters).length;
+  const thisIdx = (init >= 1 && init <= (length + 1)) ? init : random(length);
+  const thisChapter = chapters[thisIdx];
   return {
     idx: thisIdx,
     content: thisChapter
@@ -18,9 +51,26 @@ var random_chapter = function (init) {
 }
 
 if (require.main) {
-  var chapter = random_chapter(process.argv[2])
-  console.log(`Stray Birds - Chapter ${chapter.idx} \r\nRabindranath Tagore\r\n ${chapter.content}`);
-  process.exit()
+
+  (async () => {
+
+    try {
+
+      const chapter = random_chapter(await getChapters(), parseInt(process.argv[2]))
+      console.log(`\n${"Stray Birds".green}\nAuthor: ${"Rabindranath Tagore".cyan}\nChapter: ${String(chapter.idx).random} \n${chapter.content}\n`);
+      process.exit()
+
+    } catch (error) {
+
+      console.error(error)
+      process.exit(1)
+
+    }
+
+  })()
+
+
+
 }
 else {
   module.exports = random_chapter;
